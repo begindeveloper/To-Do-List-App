0f2bbde5f4404ab5c3823e5fb29cb1c587b8b913
begindeveloper <107010469+begindeveloper@users.noreply.github.com>
2024-02-22 21:47:17 -0600

    commit 02-22-2024

---
+++ script.js
@@ -0,0 +1,8 @@
+const taskInput= document.getElementById('taskInput');
+const addTask =document.getElementById('addTask');
+const tasksList= document.getElementById('tasksList');
+let tasks=[];
+
+addButton.addEventListner('click', () => {
+    const task = taskInput.ariaValueMax.trim();
+})
